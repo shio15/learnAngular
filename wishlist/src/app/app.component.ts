e1bdf70f3791eea370e4b8b0aae6e0e172ce5713
shio15 <shiozhou15@gmail.com>
2024-10-20 17:47:58 -0500

    ref

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
